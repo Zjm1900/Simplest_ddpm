14583ed51178c37100d9a3397ade3a4253553535
Zjm1900 <jiemingzhong1900@gmail.com>
2023-04-30 20:42:13 +0800

    finish model

--- DiffusionFreeGuidence/DiffusionCondition.py
+++ DiffusionFreeGuidence/DiffusionCondition.py
@@ -26,3 +26,63 @@ class GaussianDiffusionTrainer(nn.module):
         self.register_buffer(
             "sqrt_one_minus_alphas_bar", torch.sqrt(1 - alphas_bar))
         
+    def forward(self, x_0, labels):
+        '''
+        Algorithm 1
+        '''
+        t = torch.randint(self.T, size=(x_0.shape[0],), device=x_0.device)
+        noise = torch.randn_like(x_0)
+        x_t = extract(self.alpha_bar, t, x_0.shape) * x_0 + \
+              extract(self.sqrt_one_minus_alphas_bar, t, x_0.shape) * noise
+        loss = F.mse_loss(self.model(x_t, t, labels), noise, reduction='none')
+        return loss
+    
+class GaussianDiffusionSampler(nn.module):
+    def __init__(self, model, beta_1, beta_T, T, w=0.):
+        self.model = model
+        self.T = T
+        ### In the classifier free guidence paper, w is the key to control the gudience.
+        ### w = 0 and with label = 0 means no guidence.
+        ### w > 0 and label > 0 means guidence. Guidence would be stronger if w is bigger.
+
+        self.w = w
+
+        self.register_buffer('betas', torch.linspace(beta_1, beta_T, T).double())
+        alphas = 1 - self.betas
+        alphas_bar = torch.cumprod(alphas, dim=0)
+        alphas_bar_prev = F.pad(alphas_bar, [1, 0], value=1)[:T]
+        self.register_buffer('coeff1', torch.sqrt(1. / alphas_bar))
+        self.register_buffer('coeff2', self.coeff1 * (1. - alphas) / torch.sqrt(1. - alphas_bar))
+        self.register_buffer('posterior_var', self.betas * (1. - alphas_bar_prev) / (1. - alphas_bar))
+    
+    def predict_xt_prev_mean_from_eps(self, x_t, t, eps):
+        assert x_t.shape == eps.shape
+        return extract(self.coeff1, t, x_t.shape) * x_t - extract(self.coeff2, t, x_t.shape) * eps
+    
+    def p_mean_variance(self, x_t, t, labels):
+        
+        var = torch.cat([self.posterior_var[1:2], self.betas[1:]])
+        var = extract(var, t, x_t.shape)
+        eps = self.model(x_t, t, labels)
+        nonEps = self.model(x_t, t, torch.zeros_like(labels).to(labels.device))
+        eps = (1. + self.w) * eps - self.w * nonEps
+        xt_prev_mean = self.predict_xt_prev_mean_from_eps(x_t, t, eps)
+        return xt_prev_mean, var
+    
+    def forward(self, x_T, labels):
+        '''
+        Algorithm 2
+        '''
+        x_t = x_T
+        for time_step in reversed(range(self.T)):
+            print(time_step)
+            t = x_t.new_ones([x_T.shape[0]], dtype=torch.long) * time_step
+            mean, var = self.p_mean_variance(x_t, t, labels)
+            if time_step > 0:
+                noise = torch.randn_like(x_t)
+            else:
+                noise = 0
+            x_t = mean + torch.sqrt(var) * noise
+            assert torch.isnan(x_t).int().sum() == 0
+        x_0 = x_t
+        return torch.clip(x_0, -1, 1)
